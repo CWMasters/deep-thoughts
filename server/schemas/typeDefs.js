48486b15027b06179d56de0b4e2d570688d96f6e
CWMasters <95546410+CWMasters@users.noreply.github.com>
2022-05-10 20:27:26 -0700

    mutations for thoughts/friends/reactions

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -19,6 +19,11 @@ const typeDefs = gql`
         username: String
     }
 
+    type Auth {
+        token: ID!
+        user: User
+    }
+
     type User {
         _id: ID
         username: String
@@ -29,11 +34,22 @@ const typeDefs = gql`
     }
 
     type Query {
+        me: User
         users: [User]
         user(username: String!): User
         thoughts(username: String): [Thought]
         thought(_id: ID!): Thought
     }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addThought(thoughtText: String!): Thought
+        addReaction(thoughtId: ID!, reactionBody: String!): Thought
+        addFriend(friendId: ID!): User
+    }
+
+
   `;
 
 // export the typeDefs
